1ac5667536f2afe7846c2f00bfc44a64150bd548
ooookubo.48@gmail.com <ec2-user@ip-172-31-20-111.ec2.internal>
2021-05-17 07:02:34 +0000

    [Finish8]

--- bookers1/config/routes.rb
+++ bookers1/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root :to => 'books#top'
-  get 'top' => 'books#top'
+  root :to => 'homes#index'
+  resources :homes
   resources :books
+
 
 
 end
